d09215a8002657bad741ea3cc493b92236a317d0
Eman-Gon <emanpiece@gmail.com>
2024-04-24 17:15:43 -0700

    dcsnjdn

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -9,37 +9,31 @@ def get_catalog():
     """Each unique item combination must have only a single price."""
     with db.engine.begin() as connection:
         catalog = []
-        result = connection.execute(sqlalchemy.text("SELECT num_red_ml, num_green_ml, num_blue_ml FROM global_inventory")).one()
+        result = connection.execute(sqlalchemy.text("SELECT * FROM potion_inventory"))
+        numItems = 0
+
+        for x in result:
+            if(x.quantity > 0):
+                catalog.append({
+                    "sku": x.sku,
+                    "name": x.sku,
+                    "quantity": x.quantity,
+                    "price": x.price,
+                    "potion_type": [
+                        x.num_red_potions,
+                        x.num_green_potions,
+                        x.num_blue_potions,
+                        x.num_dark_potions,
+                    ]
+
+                })
+                numItems += 1
+                if (numItems == 6):
+                    break
+
+
 
         
-
-        if result.num_red_ml > 0:
-            catalog.append({
-                "sku": "RED_POTION_0",
-                "name": "red",
-                "quantity": 40,
-                "price": 50,
-                "potion_type": [100, 0, 0, 0]
-            })
-
-        if result.num_green_ml > 0:
-            catalog.append({
-                "sku": "GREEN_POTION_0",
-                "name": "green",
-                "quantity": 40,
-                "price": 50,
-                "potion_type": [0, 100, 0, 0]
-            })
-
-        if result.num_blue_ml > 0:
-            catalog.append({
-                "sku": "BLUE_POTION_0",
-                "name": "blue",
-                "quantity": 40,
-                "price": 50,
-                "potion_type": [0, 0, 100, 0]
-            })
-
         return catalog
         
         # unique_potions = set()
