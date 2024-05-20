d7620df81e9a735d80e422e863a6afd3e3708005
Eman-Gon <emanpiece@gmail.com>
2024-05-19 20:50:59 -0700

    fix bottler logic

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -1,6 +1,7 @@
 import sqlalchemy
 from src import database as db
 from fastapi import APIRouter
+from src.api.bottler import get_bottle_plan
 
 router = APIRouter()
 
@@ -8,19 +9,40 @@ router = APIRouter()
 def get_catalog():
     """Each unique item combination must have only a single price."""
     with db.engine.begin() as connection:
+        made_pot = get_bottle_plan()
+        print("Bottler Plan: ")
+        print(made_pot)
+        made_as_dictionary = {}
+        for p in made_pot:
+            sku = connection.execute(sqlalchemy.text("SELECT sku FROM potion_inventory WHERE potion_type = :potion_type"), {"potion_type": p["potion_type"]}).scalar_one()
+            made_as_dictionary[sku] = p["quantity"]
+
         catalog = []
-        result = connection.execute(sqlalchemy.text("SELECT * FROM potion_inventory_view"))
+        result = connection.execute(sqlalchemy.text("""
+            SELECT price, sku, potion_type, quantity
+            FROM potion_inventory_view
+            WHERE sku != 'dark_green'
+            ORDER BY (CASE WHEN sku IN ('green', 'red', 'blue', 'dark') THEN 0 ELSE random() + 1 END)
+            LIMIT 6
+        """))
+
         numItems = 0
         for x in result:
-            if x.quantity > 0:
+            if x.sku in made_as_dictionary.keys():
+                just_made = made_as_dictionary[x.sku]
+            else:
+                just_made = 0
+
+            if x.quantity + just_made > 0:
                 catalog.append({
                     "sku": x.sku,
                     "name": x.sku,
-                    "quantity": x.quantity,
+                    "quantity": x.quantity + just_made,
                     "price": x.price,
                     "potion_type": x.potion_type
                 })
                 numItems += 1
-                if numItems == 6:
-                    break
+            if numItems == 6:
+                break
+
         return catalog
